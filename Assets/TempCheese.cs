52ae319043c1e94f4e535c7dc61963b5dc6371dd
Remaek <brooksmt@sheridancollege.ca>
2026-05-10 14:33:10 -0400

    Scale Finished

---
+++ Assets/TempCheese.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TempCheese : MonoBehaviour
+{
+
+    public float tempCheeseWeight;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
